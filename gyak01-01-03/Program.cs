665c4e788ca21a3644cd66c8c72c4d78e6ca1b8f
Csaba Gyuris <gyuris.csaba@vasvari.org>
2021-01-27 10:02:16 +0100

    Kiadvány projekt feladatokkal.

---
+++ gyak01-01-03/Program.cs
@@ -0,0 +1,20 @@
+﻿// 3.KiadvanyokProjekt
+// Készítsen különböző kiadványokkal (pl. tankönyv, atlasz, enciklopédia,…) kapcsolatos osztályokat,
+// amelyeket egy iskolai diákjai kivesznek a könyvtárból, kinyitják és olvassák, becsukják és visszaviszik a könyvtárba.
+// Példányosítsa János és Juliska objektumokat és modellezze egy napjukat!
+// Mind a ketten délután tanulnak, ezért különböző kiadványokat kell kikölcsönözzenek, tanuljanak a kiadványokból,
+// és természetesen a könyveket vissza kell jutassák a könyvtárba.
+// Az elkészült osztály dokumentálja UML diagrammal!
+
+using System;
+
+namespace gyak01_01_03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
